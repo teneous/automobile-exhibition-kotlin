9b9714a9feca75c3db15ac453533362d8b7bf032
teneous <kevinto@foxmail.com>
2018-03-29 10:09:26 +0800

    模拟JSON提交

--- src/main/kotlin/ecommerce/databean/OrderInfo.kt
+++ src/main/kotlin/ecommerce/databean/OrderInfo.kt
@@ -1,5 +1,8 @@
 package ecommerce.databean
 
+import com.sun.xml.internal.ws.developer.Serialization
+
+@Serialization
 data class OrderInfo(
     var productList: ProductInfo? = null,
     var customerId: Long? = null
--- src/main/kotlin/ecommerce/resource/OrderProductResource.kt
+++ src/main/kotlin/ecommerce/resource/OrderProductResource.kt
@@ -3,25 +3,18 @@ package ecommerce.resource
 import ecommerce.databean.OrderInfo
 import ecommerce.service.IPlaceOrderService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
-@RestController("/v1/order")
+@RestController
+@RequestMapping("/v1/order")
 class PlaceOrder{
 
     @Autowired
     private lateinit var placeOrderService: IPlaceOrderService
 
-    @GetMapping("/placeorder")
+    @PostMapping("/placeorder")
     fun placeOrder(@RequestBody orderInfo: OrderInfo):String{
         placeOrderService.placeOrder(orderInfo)
         return "hellow"
-    }
-
-    @GetMapping("/fuck")
-    fun asd():String{
-        return "fucker"
     }
 }
